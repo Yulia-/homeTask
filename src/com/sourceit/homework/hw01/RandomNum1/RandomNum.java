651b8201765324fa59887d07e7d06ff8cdce9663
Yulia- <juliam7777@mail.ru>
2015-02-13 18:53:49 +0200

    Original commit

---
+++ src/com/sourceit/homework/hw01/RandomNum1/RandomNum.java
@@ -0,0 +1,26 @@
+package com.sourceit.homework.hw01.RandomNum1;
+
+/**
+ * Created by Юля on 13.02.2015.
+ */
+///// After compilation I got this msg: Process finished with exit code 1
+import java.util.*;
+public class RandomNum {
+    public static void main(String[] args) {
+
+        System.out.println ("Write down a number:");
+        Scanner scn = new Scanner (System.in);
+        int a = scn.nextInt();
+
+        Random r = new Random();
+        for(int i = -a; i<-9999; i++); {
+            System.out.println( r.nextInt(a) );
+        }
+
+
+
+
+    }
+
+
+}
