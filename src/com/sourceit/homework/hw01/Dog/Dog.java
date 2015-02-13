1e69c05d4d3d42c264b9535449a0cb719e27a58e
Yulia- <juliam7777@mail.ru>
2015-02-13 19:54:06 +0200

    Original commit

---
+++ src/com/sourceit/homework/hw01/Dog/Dog.java
@@ -0,0 +1,23 @@
+package com.sourceit.homework.hw01.Dog;
+
+/**
+ * Created by Юля on 13.02.2015.
+ */
+/// I didn't understand tast exactly( I wrote something like this
+public class Dog {
+
+    String str1 = new String ("name");
+    String str2 = new String ("says");
+
+
+    public static void main (String [] args){
+
+        String Dog1 = new String ("spot says “Ruff!“");
+        String Dog2 = new String ("scruffy says “Wurf!“");
+
+        System.out.println (Dog1);
+        System.out.println (Dog2);
+
+
+    }
+}
