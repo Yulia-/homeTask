b2d0f5b7926a5b2ea320d350ae6dab83b5c143d9
Yulia- <juliam7777@mail.ru>
2015-02-14 20:15:33 +0200

    Original commit

--- src/com/sourceit/homework/hw01/RandomNumber/RandomNumber.java
+++ src/com/sourceit/homework/hw01/RandomNumber/RandomNumber.java
@@ -4,39 +4,32 @@ package com.sourceit.homework.hw01.RandomNumber;
 /**
  * Created by Юля on 13.02.2015.
  */
-
-//Something doesn't work in this program
-
 import java.util.*;
 
 public class RandomNumber {
     public static void main(String[] args) {
 
         Random r = new Random();
-        for (int i = 0; i < 5; i++) ;
+        for (int i = 100; i < 101; i++) ;
 
         System.out.println(r.nextInt(999));
 
+        int s = r.nextInt ();
 
-
-        int a = r.nextInt / 100;
-        int b = r.nextInt % 100 / 10;
-        int c = r.nextInt % 100 - b;
+        int a = s % 10;;
+        int b = s / 100;
+        int c = (s % 100) -a;
 
         if( a > b ) {
             if (a > c){
                 System.out.println (a);
-            }
-            else {
+            } else {
                 System.out.println (c);
             }
-
-        }
-        else {
+        } else {
             if (b > c){
                 System.out.println (b);
-            }
-            else {
+            } else {
                 System.out.println (c);
             }
         }
