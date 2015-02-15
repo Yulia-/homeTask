4bf8af322d0df6c1bcf70d37e1742e4609ba68e5
Yulia- <juliam7777@mail.ru>
2015-02-15 23:41:17 +0200

    Original commit

--- src/com/sourceit/homework/hw01/HomeWork03/Clock.java
+++ src/com/sourceit/homework/hw01/HomeWork03/Clock.java
@@ -5,15 +5,13 @@ package com.sourceit.homework.hw01.HomeWork03;
  */
 public class Clock {
     public static void main (String [] args){
-        int n1, n2, n3, n4, k=0;
-        for (n1=0; n1<3; n1++)
-            for (n2=0; n2<10; n2++)
-                for (n3=0; n3<6; n3++)
-                    for (n4=0; n4<10; n4++){
-                        if (n1==n4)
-                            if(n2==n3)
-                                k++;
-                    }
+        int n1, n2, k=0;
+        for (n1=0; n1<24; n1++)
+            for (n2=0; n2<60; n2++)
+                if (n1/10 == n2%10 & n1%10 == n2/10)
+                    k++;
         System.out.println (k);
-    }
+                    }
+    
 }
+
