24e38e1d988042695c64e1d4e875548cb2bc5df8
Yulia- <juliam7777@mail.ru>
2015-02-16 19:54:14 +0200

    Original commit

---
+++ src/com/sourceit/homework/hw01/HomeWork03/Array7x4.java
@@ -0,0 +1,36 @@
+package com.sourceit.homework.hw01.HomeWork03;
+
+import java.util.*;
+public class Array7x4 {
+    public static void main (String [] args) {
+
+
+        int max = 0;
+        int 1 = 0;
+
+        int[][] arr = new int[7][4];
+        Random r = new Random();
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[i][j] = r.nextInt(11)-5;
+                System.out.println(arr[7][4]);
+                System.out.println( );
+
+                int [] a = new int [7];
+                for (int k=0; k<7; k++)
+                    a[k]*=arr [i][j];
+                for (int k=0;k<7; k++ ) {
+                    max = Math.abs(a[k]);
+                    if (Math.abs(a[k]) < max)
+                        max = Math.abs(a[k]);
+                    i = k;
+                    System.out.println (k);
+                }
+
+            }
+        }
+
+
+    }
+}
