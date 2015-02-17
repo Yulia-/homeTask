5486bb54bfcef298c8858883425c18307efbc87b
Yulia- <juliam7777@mail.ru>
2015-02-17 15:26:50 +0200

    Original commit

--- src/com/sourceit/homework/hw01/HomeWork03/Array7x4.java
+++ src/com/sourceit/homework/hw01/HomeWork03/Array7x4.java
@@ -2,35 +2,34 @@ package com.sourceit.homework.hw01.HomeWork03;
 
 import java.util.*;
 public class Array7x4 {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
 
+        int max = 1;
+        int i = 7;
+        int j = 4;
+        int[][] arr = new int[i][j];
 
-        int max = 0;
-        int 1 = 0;
-
-        int[][] arr = new int[7][4];
         Random r = new Random();
 
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 4; j++) {
-                arr[i][j] = (r.nextInt(11)-5);
-                System.out.println(arr[7][4]);
-                System.out.println( );
+        for (i = 0; i < 7; i++) {
+            for (j = 0; j < 4; j++) {
+                arr[i][j] = (r.nextInt(11) - 5);
 
-                int [] a = new int [7];
-                for (int k=0; k<7; k++)
-                    a[k]*=arr [i][j];
-                for (int k=0;k<7; k++ ) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int[] a = new int[7];
+        for (int k = 0; k < 7; k++) {
+            a[k] *= arr[i][j];
+            for (k = 0; k < 7; k++) {
+                if (Math.abs(a[k]) < max) {
                     max = Math.abs(a[k]);
-                    if (Math.abs(a[k]) < max)
-                        max = Math.abs(a[k]);
-                    i = k;
-                    System.out.println (k);
+                    k = i;
                 }
-
+                System.out.println(k);
             }
         }
-
 
     }
 }
