7bfde7f48132c40e1ee22d1c7a8341af25f45d65
Yulia- <juliam7777@mail.ru>
2015-02-20 20:24:33 +0200

    Original commit

---
+++ src/com/sourceit/homework/hw01/HomeWork03/Recursion/PrimeNumbers.java
@@ -0,0 +1,30 @@
+package com.sourceit.homework.hw01.HomeWork03.Recursion;
+
+import java.util.Scanner;
+
+/**
+ * Created by Юля on 20.02.2015.
+ */
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Write down a number:");
+        int n = scr.nextInt();
+        System.out.println(prime(n));
+
+    }
+    static int prime( int n){
+        for (int i = 2; i < n; i++) {
+
+            if (n == 2)
+                return 2;
+            else if ((i%prime(n-1))!=0)
+                return i;
+            else continue;
+        }
+    }
+
+
+}
+
