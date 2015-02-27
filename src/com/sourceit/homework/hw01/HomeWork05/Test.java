c6662be1a97757fccdb4c5f2717aa0a49a63a380
Yulia- <juliam7777@mail.ru>
2015-02-27 22:49:49 +0200

    Original commit

--- src/com/sourceit/homework/hw01/HomeWork05/Test.java
+++ src/com/sourceit/homework/hw01/HomeWork05/Test.java
@@ -1,20 +1,22 @@
 package com.sourceit.homework.hw01.HomeWork05;
 
-import com.sourceit.hometask.basic.*;
-
 /**
  * Created by Юля on 27.02.2015.
  */
 public class Test {
     public static void main (String [] args){
 
-        Test FractionNumber = new com.sourceit.hometask.basic.FractionNumber;
+        FractionNumber var1 = new FractionNumber();
 
         var1.setDividend(2);
         var1.setDivisor(5);
+        System.out.println(var1);
+
+        FractionNumber var2 = new FractionNumber();
         var2.setDividend(1);
         var2.setDivisor(3);
-        System.out.println(FractionNumber);
+
+        System.out.println(var2);
 
     }
 }
