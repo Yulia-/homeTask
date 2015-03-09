1f7bb7365b792b6d00da18b7cd416080debabd04
Yulia- <juliam7777@mail.ru>
2015-03-09 18:29:34 +0200

    Original commit

--- src/com/sourceit/homework/hw01/HomeWork06/StringUtilsImpl.java
+++ src/com/sourceit/homework/hw01/HomeWork06/StringUtilsImpl.java
@@ -1,5 +1,4 @@
 package com.sourceit.homework.hw01.HomeWork06;
-
 import com.sourceit.hometask.exceptions.CustomException;
 import com.sourceit.hometask.exceptions.StringUtils;
 
@@ -9,42 +8,73 @@ import com.sourceit.hometask.exceptions.StringUtils;
 public class StringUtilsImpl implements StringUtils {
 
 
-
     @Override
     public double div(String var1, String var2) throws NullPointerException, NumberFormatException, ArithmeticException {
-        if (var1 == null){
+        if (var1 == null) {
             throw new NullPointerException("Value of the first number is null");
         }
-        if (var2 == null){
+        if (var2 == null) {
             throw new NullPointerException("Value of the second number is null");
         }
         double number1 = Double.parseDouble(var1);
         try {
             number1 = Double.parseDouble(var1);
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("Written first variable is not a number");
         }
 
         double number2 = Double.parseDouble(var2);
         try {
             number2 = Double.parseDouble(var2);
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("Written second variable is not a number");
         }
 
-        if (number2==0){
+        if (number2 == 0) {
             throw new ArithmeticException("Divisor can't be 0");
         }
-        return number1/number2;
+        return number1 / number2;
     }
 
     @Override
-    public int[] findWord(String s, String s1) throws NullPointerException {
-        return new int[0];
+    public int[] findWord(String text, String word) throws NullPointerException {
+        int k = 0;
+        int[] mass2 = new int[k];
+        if (text == null) {
+            throw new NullPointerException("Text is null");
+        }
+        if (word == null) {
+            throw new NullPointerException("Word is null");
+        }
+        String [] mass = text.split(" ");
+
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i].equals(word)) {
+                for (k = 0; k < mass.length; k++) {
+                    mass2[k] = i;
+                }
+            }
+        } return mass2;
     }
 
     @Override
-    public double[] findNumbers(String s) throws CustomException {
-        return new double[0];
+    public double[] findNumbers(String text2) throws CustomException {
+        double number;
+        String [] massForDouble = text2.split(" ");
+        int k = 0;
+        double [] massDouble = new double [k];
+
+        for (int i=0; i<massForDouble.length; i++) {
+            number = Double.parseDouble(massForDouble[i]);
+            for (k=0; k<massForDouble.length; k++ )
+                massDouble[k] = number;
+        }
+        if (massDouble[k] == 0){
+            throw new CustomException("No double value was found in the text");
+        }
+
+        return  massDouble;
+
+
     }
 }
--- src/com/sourceit/homework/hw01/HomeWork06/Test.java
+++ src/com/sourceit/homework/hw01/HomeWork06/Test.java
@@ -1,12 +1,15 @@
 package com.sourceit.homework.hw01.HomeWork06;
 
+import com.sourceit.hometask.exceptions.CustomException;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by Юля on 03.03.2015.
  */
 public class Test {
-    public static void main (String [] args){
+    public static void main (String [] args) throws CustomException {
 
         StringUtilsImpl variable = new StringUtilsImpl();
         Scanner scr = new Scanner(System.in);
@@ -15,6 +18,14 @@ public class Test {
         System.out.println("Write down the second number: ");
         String var2 = scr.nextLine();
         System.out.println("Result of division: " + variable.div(var1, var2));
+
+
+        String text = "We are beautiful";
+        String word = "We";
+        System.out.println(Arrays.toString(variable.findWord(text, word)));
+
+        String text2 = "she is 22.5 years old";
+        System.out.println(Arrays.toString(variable.findNumbers(text2)));
 
     }
 
